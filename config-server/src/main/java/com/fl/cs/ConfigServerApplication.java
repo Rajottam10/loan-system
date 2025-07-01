288231f284d7763e967dc6c54aa9d5d0dd746d7f
Rajottam Pradhan <rajottampradhan@gmail.com>
2025-07-01 15:11:23 +0545

    Added Config-server and other dependencies

--- config-server/src/main/java/com/fl/cs/ConfigServerApplication.java
+++ config-server/src/main/java/com/fl/cs/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.fl.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
--- fl-auth-server/src/main/java/com/fl/flas/config/AuthorizationServerConfig.java
+++ fl-auth-server/src/main/java/com/fl/flas/config/AuthorizationServerConfig.java
@@ -1,28 +1,80 @@
 package com.fl.flas.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfig {
     @Bean
-    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception{
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/oauth2/**", "/actuator/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated())
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/oauth2/**"))
-                        .formLogin(Customizer.withDefaults())
-                        .oauth2ResourceServer(oauth2 -> oauth2
-                                .jwt(Customizer.withDefaults()));
-        return http.build();
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.formLogin(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(){
+        RegisteredClient registeredClient =  RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId("central-system")
+                .clientSecret("{noop}secret123")
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope("read")
+                .scope("write")
+                .build();
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings(){
+        return AuthorizationServerSettings.builder()
+                .issuer("http://localhost:9000")
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        RSAKey rsaKey = generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    private static RSAKey generateRsa() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+            return new RSAKey.Builder(publicKey)
+                    .privateKey(privateKey)
+                    .keyID(UUID.randomUUID().toString())
+                    .build();
+
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 }
